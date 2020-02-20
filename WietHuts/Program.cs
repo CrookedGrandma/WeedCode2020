bdaed820f768c3debec5195f9290a62d98a54212
Mstiekema <merijnstiekema@outlook.com>
2020-02-20 19:02:27 +0100

    Added input reader

--- WietHuts/Program.cs
+++ WietHuts/Program.cs
@@ -9,7 +9,28 @@ namespace WietHuts {
         }
 
         static void ReadInput() {
+            // Get the info
+            string info = Console.WriteLine();
+            int books = int.Parse(info[0]);
+            int libraries = int.Parse(info[1]);
+            int days = int.Parse(info[2]);
 
+            // Get book scores
+            string bookScoreString = Console.WriteLine();
+            string[] splitScores = bookScoreString.Split(" ");
+            int[] bookScores = new int[books];
+            for (int i = 0; i < books; i++)
+                bookScores[i] = splitScores[i];
+
+            // Info per library
+            for (int i = 0; i < libraries; i++) {
+                string[] libInfo = Console.WriteLine().Split(" ");
+                int[] bookInLib = Console.WriteLine().Split(" ").Select(x => int.Parse(x));
+
+                int bookAmount = int.Parse(libInfo[0]);
+                int signUpProc = int.Parse(libInfo[1]);
+                int shipPerDay = int.Parse(libInfo[2]);
+            }
         }
 
         static void DoThings() {
